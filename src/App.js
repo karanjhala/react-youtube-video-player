8505b3dc752547f5203f5f3b5930794a29eef798
karanjhala <karanjhala@gmail.com>
2020-11-18 12:51:32 +0800

    add react-router-dom, create components & use link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter,Route} from 'react-router-dom'
+import Home from './components/Home';
+import CourseStructure from './components/CourseStructure';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route path="/" component={Home} />
+        <Route path="/:coursename" component={CourseStructure} />
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/components/CourseStructure.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+import {Link} from 'react-router-dom';
+
+function CourseStructure(props) {
+  return (
+    <div className="App">
+      <Link to="/reactjs"> 
+        <h1>ReactJs course</h1>
+      </Link>
+      <Link to="/nodejs"> 
+        <h1>NodeJS course</h1>
+      </Link>
+      <h1>KVSJ</h1>
+      
+    </div>
+  );
+}
+
+export default CourseStructure;
